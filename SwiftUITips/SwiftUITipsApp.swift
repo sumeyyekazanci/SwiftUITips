c03009da39244dd863e7bb10a08a86d1b0b5a17a
Sümeyye Kazancı <sumeyye.kazanci@btpro.net>
2023-05-20 19:53:21 +0300

    Initial Commit

---
+++ SwiftUITips/SwiftUITipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITipsApp.swift
+//  SwiftUITips
+//
+//  Created by Sümeyye Kazancı on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
