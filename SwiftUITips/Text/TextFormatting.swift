428a2130b95f73c817a0d5e4b56dbe63e05388fc
Sumeyye Kazanci <sumeyye.kazanci@btpro.net>
2023-07-02 23:50:22 +0300

    Add text formatting

---
+++ SwiftUITips/Text/TextFormatting.swift
@@ -0,0 +1,41 @@
+//
+//  TextFormatting.swift
+//  SwiftUITips
+//
+//  Created by Sümeyye Kazancı on 2.07.2023.
+//
+
+import SwiftUI
+
+struct TextFormatting: View {
+    @State private var modifierActive = true
+    
+    var body: some View {
+        
+        
+        VStack {
+            
+            Toggle("Modifiers Active", isOn: $modifierActive)
+                .font(.title)
+                .padding(.horizontal)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Bold").bold()
+                Text("Italic").italic()
+                Text("Strikethrough").strikethrough()
+                Text("Green Strikethrough")
+                    .strikethrough(modifierActive, color: .red)
+                Text("Text Color (ForegroundColor)").foregroundColor(.indigo)
+                Text("Underline").underline()
+                Text("Red Underline").underline(modifierActive, color: .red)
+            }
+            .font(.title)
+        }
+    }
+}
+
+struct TextFormatting_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFormatting()
+    }
+}
