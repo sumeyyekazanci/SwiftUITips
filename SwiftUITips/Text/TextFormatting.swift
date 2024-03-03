907c0a667bdcacb03c90ffe32dea455322d89571
Sümeyye Kazancı <sumeyye.kazanci@btpro.net>
2024-03-03 12:58:34 +0300

    Add adapted text color.

--- SwiftUITips/Text/TextFormatting.swift
+++ SwiftUITips/Text/TextFormatting.swift
@@ -8,29 +8,35 @@
 import SwiftUI
 
 struct TextFormatting: View {
-    @State private var modifierActive = true
+    @State private var modifierActive = false
+    @State private var color: Color = .white
     
     var body: some View {
-        
-        
         VStack {
-            
             Toggle("Modifiers Active", isOn: $modifierActive)
                 .font(.title)
                 .padding(.horizontal)
+                .onChange(of: modifierActive) { value in
+                    modifierActive = value
+                    color = modifierActive ? .black : .white
+                }
             
-            VStack(alignment: .leading, spacing: 10) {
-                Text("Bold").bold()
-                Text("Italic").italic()
-                Text("Strikethrough").strikethrough()
-                Text("Red Strikethrough")
-                    .strikethrough(modifierActive, color: .red)
-                Text("Text Color (ForegroundColor)").foregroundColor(.indigo)
-                Text("Underline").underline()
-                Text("Red Underline").underline(modifierActive, color: .red)
+        ScrollView {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Bold").bold()
+                    Text("Italic").italic()
+                    Text("Strikethrough").strikethrough()
+                    Text("Red Strikethrough")
+                        .strikethrough(modifierActive, color: .red)
+                    Text("Text Color (ForegroundColor)").foregroundColor(.indigo)
+                    Text("Underline").underline()
+                    Text("Red Underline").underline(modifierActive, color: .red)
+                }
+                .font(.title)
             }
-            .font(.title)
         }
+        .foregroundStyle(color.adaptedTextColor())
+        .background(color)
     }
 }
 
--- SwiftUITips/Text/TextTips.swift
+++ SwiftUITips/Text/TextTips.swift
@@ -9,54 +9,48 @@ import SwiftUI
 
 struct TextTips: View {
     var body: some View {
-        VStack(spacing: 20) {
-            // From a String Literal
-            Text("Hello,**world**! Check out our [website](https://example.com)")
-                .font (.title)
-            
-            // From a String Literal with tint color
-            Text("Hello,**world**! Check out our [website](https://example.com)")
-                .tint(.orange)
-                .font (.title)
-            
-            // From a variable
-            let exampleString = "Hello,**world**! Check out our [website](https://example.com)"
-            
-            // Does not work
-            Text(exampleString)
-                .font (.title)
-            
-            // Works
-            Text(LocalizedStringKey(exampleString))
-                .font (.title)
-            
-            // From an AttributedString (no localization)
-            if let attrStr = try? AttributedString (markdown: exampleString) {
-                Text (attrStr)
-                    .font (.title)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 20) {
+                // From a String Literal
+                Text("Hello,**world**! Check out our [website](https://example.com)")
+                
+                // From a String Literal with tint color
+                Text("Hello,**world**! Check out our [website](https://example.com)")
+                    .tint(.orange)
+                
+                // From a variable
+                let exampleString = "Hello,**world**! Check out our [website](https://example.com)"
+                
+                // Does not work
+                Text(exampleString)
+                
+                // Works
+                Text(LocalizedStringKey(exampleString))
+                
+                // From an AttributedString (no localization)
+                if let attrStr = try? AttributedString (markdown: exampleString) {
+                    Text (attrStr)
+                }
+                
+                // Custom actions for link inside a text
+                Text("Hello,**world**! Check out our [website](https://example.com)")
+                    .environment(\.openURL, OpenURLAction(handler: { url in
+                        print("Link tapped: \(url)")
+                        return .handled
+                    }))
+                
+                // Interpolate text with text modifiers
+                Text("Hello \(Text("world").foregroundColor(.orange))")
+                
+                let eventDate = Date(timeIntervalSinceNow: 646)
+                // Date updates automatically in the UI
+                Text ("\(eventDate, style: .relative) left until the event")
+                
+                // Fixed width
+                Text ("\(eventDate, style: .relative) left until the event")
+                    .monospacedDigit()
             }
-            
-            // Custom actions for link inside a text
-            Text("Hello,**world**! Check out our [website](https://example.com)")
-                .environment(\.openURL, OpenURLAction(handler: { url in
-                    print("Link tapped: \(url)")
-                    return .handled
-                }))
-                .font (.title)
-            
-            // Interpolate text with text modifiers
-            Text("Hello \(Text("world").foregroundColor(.orange))")
-                .font (.title)
-            
-            let eventDate = Date(timeIntervalSinceNow: 646)
-            // Date updates automatically in the UI
-            Text ("\(eventDate, style: .relative) left until the event")
-            .font (.title)
-            
-            // Fixed width
-            Text ("\(eventDate, style: .relative) left until the event")
-                .monospacedDigit()
-                .font (.title)
+            .font(.system(size: 24))
         }
     }
 }
